92428553a567df4d57e42804de5e1c23ee4a611b
Ludvig Delvret <ludvig.delvret.2330@student.lu.se>
2022-02-22 19:21:45 +0100

    spinning and changing size

--- react/order-system/src/App.js
+++ react/order-system/src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import Button from '@mui/material/Button';
+
 
 function App() {
+  const [number, setNumber] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {number}
         </p>
+        <Button variant="contained" size="large" onClick={() => setNumber(number+1)}>öka</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
